ceb3e1ea498f3fbddb2b2a04e0cb941ed066f0b2
Nikolaj Brask-Nielsen <nikolajgive@gmail.com>
2022-02-19 09:56:03 +0100

    chore: Release persistence project

--- release.bat
+++ release.bat
@@ -1,1 +1,3 @@
 dotnet pack .\src\Nikcio.ApiAuthentication\Nikcio.ApiAuthentication.csproj -c release -o .\releases\nuget
+
+dotnet pack .\src\Nikcio.ApiAuthentication.Persistence\Nikcio.ApiAuthentication.Persistence.csproj -c release -o .\releases\nuget
